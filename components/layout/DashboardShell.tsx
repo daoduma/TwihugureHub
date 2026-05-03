a45ac7db0903e980c34500a5bf5dad9f3e385cf0
danielakhereoduma <u17090700@tuks.co.za>
2026-05-04 00:00:41 +0200

    Update DashboardShell.tsx

--- components/layout/DashboardShell.tsx
+++ components/layout/DashboardShell.tsx
@@ -31,25 +31,3 @@ export function DashboardShell({ children }: { children: React.ReactNode }) {
     </div>
   );
 }
-
-  return (
-    <div className="min-h-screen bg-gray-50">
-      <Navbar onMenuToggle={() => setSidebarOpen((v) => !v)} />
-      <Sidebar isOpen={sidebarOpen} onClose={() => setSidebarOpen(false)} />
-
-      {/* Main content area */}
-      <main
-        className="min-h-screen transition-all duration-300"
-        style={{
-          paddingTop: "var(--navbar-height)",
-          paddingLeft: "0px",
-        }}
-      >
-        {/* Desktop: offset for sidebar */}
-        <div className="lg:pl-[var(--sidebar-width)]">
-          <div className="p-6">{children}</div>
-        </div>
-      </main>
-    </div>
-  );
-}
